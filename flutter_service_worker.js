8c923489474a59e4ff36271a92b8e1db501868fe
equinox <aldeadeninosc@gmail.com>
2024-09-15 00:33:12 -0500

    fix: solucionar errores

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,14 +50,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3dfa2b888cdbb1eda37b7b9df7ee8f88
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "98a6ee230a89dc0cdefc7c7a873afe50",
+"flutter_bootstrap.js": "4101d2a03b503d11333acf0d0ccb7df6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "645dff6c72d1f93a30d24c4a1e8b0411",
 "/": "645dff6c72d1f93a30d24c4a1e8b0411",
-"main.dart.js": "a435968f12b81b00b8ce29b2bc6e71ba",
+"main.dart.js": "e6dde085b4cac3e238a1a779937b8b01",
 "manifest.json": "1bb1e9cd3ef3bb211c2b4bde008d8545",
 "version.json": "1d69ee52aa61bc9d567640a75dea934c"};
 // The application shell files that are downloaded before a service worker can
